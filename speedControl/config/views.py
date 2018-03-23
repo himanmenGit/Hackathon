ed613ba4ffb59111277ca411de6d79ce12994122
himanmen Git <study.himanmen@gmail.com>
2018-03-24 06:16:37 +0900

    문자 메시지 쿨타임 30분

--- speedControl/config/views.py
+++ speedControl/config/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 
 from utils.sms import SMS
@@ -22,12 +23,12 @@ def send_sms_view(request):
         if now_time.timestamp() > prev_time:
             prev_time = now_time.timestamp() + (30 * 60 * 60)
 
-            # import random
-            # # 사용할때 이렇게 사용하면 됨.
-            # send_message = ['빠름 빠름 빠름', '쉬는 쉬간 입니다', '저는 아무것도 모르겠습니다']
-            # sms = SMS(to_number='01051420203', text=random.choice(send_message))
-            #
-            # sms.send_sms()
+            import random
+            # 사용할때 이렇게 사용하면 됨.
+            send_message = ['빠름 빠름 빠름', '쉬는 시간 입니다', '저는 아무것도 모르겠습니다']
+            sms = SMS(to_number=settings.LHY_NUMBER, text=random.choice(send_message))
+
+            sms.send_sms()
         next_path = request.POST.get('next-path')
         return redirect(next_path)
 
