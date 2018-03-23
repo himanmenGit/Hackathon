ab8107b84a42e25440a76f97427878fc77b27700
himanmen Git <study.himanmen@gmail.com>
2018-03-23 17:03:21 +0900

    membsers 앱 만들고 settings.py에 AUTH_USER_MODEL members.User 추가, INSTALLED_APP에 members 추가

--- speedControl/config/settings.py
+++ speedControl/config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'membser.User'
 
 # Application definition
 
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions'
+
+    'members',
 ]
 
 MIDDLEWARE = [
