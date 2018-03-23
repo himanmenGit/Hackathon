d275f66cdc936711e6e0f0ddf7bc210c279c7bfa
himanmen Git <study.himanmen@gmail.com>
2018-03-23 20:21:57 +0900

    template view로 변경 준비

--- speedControl/config/urls/__init__.py
+++ speedControl/config/urls/__init__.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('config.urls.views')),
     path('api/', include('config.urls.apis')),
 ]
 
