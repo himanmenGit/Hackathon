b0b0e20a83dfed1c0b39404f2fb7d4f11d3b4f3d
himanmen Git <study.himanmen@gmail.com>
2018-03-24 04:07:21 +0900

    일단 유저 이름으로 멀티채팅 가능 근데 아직 로비로 들어가야함

--- speedControl/chatting/consumers.py
+++ speedControl/chatting/consumers.py
@@ -1,6 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -25,12 +26,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        user = self.scope['user']
+        name = user.nickname if user.nickname is not '' else user.username
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
+                'name': name,
                 'message': message
             }
         )
@@ -38,8 +41,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        name = event['name']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'name': name,
         }))
